7a3e007eb5c7468c156ef231597c90fc07fa54a3
robin-core360 <robin.rajput@360coreinc.com>
2022-09-03 12:52:56 +0530

    udego dapp files

---
+++ static/js/22.bd03c6ae.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[22],{1991:function(i,a){}}]);
+//# sourceMappingURL=22.bd03c6ae.chunk.js.map
